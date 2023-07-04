dcb14874fcea9f90f4a4320ea51fc1df2eecd414
adan-zeb <136443628+adan-zeb@users.noreply.github.com>
2023-07-04 22:36:04 +0500

    Update day01-challenge3.js

--- day01-challenge3.js
+++ day01-challenge3.js
@@ -1,2 +1,11 @@
 //resultVarible=2040
 //Type of resultVarible is "string"
+
+By using JavaScript's built-in Number() function, we can convert a string to a number
+const variable1 = 20;
+const variable2 = '40';
+
+const resultVariable = variable1 + Number(variable2);
+
+console.log(resultVariable); // Prints 60, and is a number
+
