a11cbdf39a5cc59562cb47759c5ee332dc536b2f
adan-zeb <136443628+adan-zeb@users.noreply.github.com>
2023-07-04 22:48:50 +0500

    Create day01-challenge5.js

---
+++ day01-challenge5.js
@@ -0,0 +1,6 @@
+ //Reason:This code is trying to print the 4th element of the array, which doesn't exist
+
+//Correction(following code prints the last-most element of the array):
+const myArray=[20, 30, 40];
+console.log(myArray[2]);
+
