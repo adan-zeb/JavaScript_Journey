f7e60508333155a9f3242125a5b097bd04f1f779
adan-zeb <136443628+adan-zeb@users.noreply.github.com>
2023-07-05 19:41:32 +0500

    Create day-2 challenge#01.js

---
+++ day-2 challenge#01.js
@@ -0,0 +1,9 @@
+//Error: the variable is declared with the keyword const, thus it cannot be incremented
+//Correction:
+let numberVariable = 0;
+numberVariable++;
+numberVariable++;
+numberVariable++;
+console.log(numberVariable);
+
+//Result: 3
